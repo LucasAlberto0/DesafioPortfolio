0cacb3688bb49322382dc42a585b6ea5f9194ad1
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-10-04 20:01:38 -0300

    feat: menu-interativo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 AOS.init();
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+const links = menu.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show");
+    });
+});
